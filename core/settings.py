0fcb466a2a5a69c110a7f1b3f1ef645168334491
Aki Taniguchi <48996356+akicestmoi@users.noreply.github.com>
2023-03-22 10:37:33 +0100

    Switched back to Render.com Redis for Channel Layers

--- core/settings.py
+++ core/settings.py
@@ -147,6 +147,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Channels
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(env("REDIS_URL"))]
+        }
     }
 }
